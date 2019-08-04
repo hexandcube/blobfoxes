d1fdc75d84619a1bf65ed2239ad8668de8e38705
Feuerfuchs <git@feuerfuchs.dev>
2019-08-04 19:58:05 +0200

    Generate index.json as well

--- gen.sh
+++ gen.sh
@@ -2,6 +2,16 @@
 
 FILELIST=`find . -type f -iname '*.png' -exec sh -c 'x=${0#./}; printf "%s:%s|" ${x%.svg.png} $x' {} \;`
 
-jq -Rn 'input | split("|") | map(split(":") | { key: .[0], value: .[1] }) | from_entries' <<<"${FILELIST%|}">blobfox.json
+jq -Rn 'input | split("|") | map(split(":") | { key: .[0], value: .[1] }) | from_entries' <<< "${FILELIST%|}" > blobfox.json
 
 zip blobfox.zip *.png
+
+CHECKSUM=`sha256sum -z blobfox.zip | awk '{ print $1 }'`
+
+printf '{
+    "src":         "https://git.pleroma.social/pleroma/emoji-index/raw/master/packs/blobfox.zip",
+    "src_sha256":  "%s",
+    "license":     "Apache 2.0",
+    "files":       "blobfox.json",
+    "description": "Like Blobcat, except with foxes"
+}' $CHECKSUM > index.json
